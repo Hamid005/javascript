7b9c35a9667bcab8eb9110e947219415629ec5c6
Hamid005 <139197553+Hamid005@users.noreply.github.com>
2024-07-15 13:52:24 +0500

    condition

---
+++ playground/basic.js
@@ -0,0 +1,11 @@
+//variable
+//var, let,const
+
+//Condition
+const marks = 45;
+if(marks < 30 ) {
+    console.log("you are fail");
+}
+else{
+    console.log('you are pass.');
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+const a = 10;
+const b = 45;
+const c = a + b;
+console.log(c);
+
+const data = [44,33,21,8,66,79,9,9,99]
+console.log(data.sort())
