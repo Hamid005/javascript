0fca886381a9e6d6f65f24d5b7d5519e14990d61
Hamid005 <139197553+Hamid005@users.noreply.github.com>
2024-07-15 14:13:07 +0500

    condition 2

--- playground/basic.js
+++ playground/basic.js
@@ -9,3 +9,10 @@ if(marks < 30 ) {
 else{
     console.log('you are pass.');
 }
+const marks1 = 45;
+if(marks <33 && marks >= 0 ){
+    console.log("you have failed")
+}
+if(marks >=33  && marks <= 50 ){
+    console.log("you have grade B")
+}
