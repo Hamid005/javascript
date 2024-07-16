3870d793e1c80e73711a4fd0a25d5a67a1471037
Hamid005 <139197553+Hamid005@users.noreply.github.com>
2024-07-16 12:03:06 +0500

    switch + exampl

--- playground/basic.js
+++ playground/basic.js
@@ -2,30 +2,56 @@
 //var, let,const
 
 //Condition
-const marks = 45;
-if(marks < 30 ) {
-    console.log("you are fail");
-}
-else{
-    console.log('you are pass.');
-}
-const marks1 = 25;
-if(marks1 <33 && marks1 >= 0 ){
-    console.log("you have failed");
-}
- else if(marks1 <=33  && marks1 <= 50 ){
-    console.log("you have grade B");
-}
+// const marks = 45;
+// if(marks < 30 ) {
+//     console.log("you are fail");
+// }
+// else{
+//     console.log('you are pass.');
+// }
+// const marks1 = 25;
+// if(marks1 <33 && marks1 >= 0 ){
+//     console.log("you have failed");
+// }
+//  else if(marks1 <=33  && marks1 <= 50 ){
+//     console.log("you have grade B");
+// }
 
-const temp = 46;
-if (temp > 45 ){
-    console.log("too hot");
-}
- else if (temp > 35){
-    console.log("prety nice ");
-}
+// const temp = 46;
+// if (temp > 45 ){
+//     console.log("too hot");
+// }
+//  else if (temp > 35){
+//     console.log("prety nice ");
+//   }
 
+// ( marks >= 33 ? console.log( " pass ")  : console.log( " fail " ) )
+//switch
 
+const number = 10 ;
+switch( number){
+    case 10:
+        console.log("yes it is 10");
+        case 20 :
+            console.log ('no its not 10');
+            break ;
+            default :
+            console.log ('this is default')
+// exampl # 2
+            const number1 = 10;
+            switch (number1 < 0){
+                case true :
+                    console.log("negative")
+                    case break :
+default :
+console.log ("positive") ;          
+}
+// exampl 2
+const name = "hamid"
+switch(name){
+    case abduallah:
+    console.log ("hello hamid")
+}
 
 
 
