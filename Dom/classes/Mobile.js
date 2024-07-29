f42218545d1fa9d908de10c0bbadc8d06b249873
Hamid005 <139197553+Hamid005@users.noreply.github.com>
2024-07-29 13:14:58 +0500

    summsung object

---
+++ Dom/classes/Mobile.js
@@ -0,0 +1,23 @@
+class Mobile{
+    name;
+    model;
+    battery;
+    os;
+    storage;
+    mic;
+
+call ( ){
+
+}
+sms(){
+
+}
+recording( ){
+ // recording up to 1 hour
+}
+charging(){
+
+}
+
+
+}
---
+++ Dom/classes/samsung.js
@@ -0,0 +1,10 @@
+ const Mobile =require( `./Mobile` );
+
+class Samsung extends Mobile { 
+    bixby( ){
+
+    }
+    recording ( ){
+        // it can upto 10 hours.
+    }
+}
